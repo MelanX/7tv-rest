8be552fe782d57fb9f124f909424f0a1e46b12f6
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-04-08 16:05:37 +0200

    Fixes

--- test/test.ts
+++ test/test.ts
@@ -1,74 +1,57 @@
 const SevenTV = require('../src');
-const api = SevenTV();
 
 describe("SevenTV Rest API", () => {
     test('Fetch user', async() => {
-        const user = await api.getUser('castcrafter');
+        const user = await SevenTV.getUser('castcrafter');
         expect(user.display_name).toBe('CastCrafter');
     });
 
     test('Fetch user with invalid username', async() => {
         try {
-            await api.getUser('invalid');
+            await SevenTV.getUser('invalid');
         } catch (e) {
             expect(e.message).toBe('User not found');
         }
     });
 
     test('Fetch emotes', async() => {
-        const emotes = await api.getEmotes('castcrafter');
+        const emotes = await SevenTV.getEmotes('castcrafter');
         expect(emotes.length).toBeGreaterThan(0);
     });
 
     test('Fetch emotes with invalid username', async() => {
         try {
-            await api.getEmotes('invalid');
+            await SevenTV.getEmotes('invalid');
         } catch (e) {
             expect(e.message).toBe('User not found');
         }
     });
 
     test('Fetch single emote', async() => {
-        const emote = await api.getEmote('60a58e67a71d9fd11049f5e9');
+        const emote = await SevenTV.getEmote('60a58e67a71d9fd11049f5e9');
         expect(emote.name).toBe('muted');
     });
 
     test('Fetch single emote with invalid id', async() => {
         try {
-            await api.getEmote('invalid');
+            await SevenTV.getEmote('invalid');
         } catch (e) {
             expect(e.message).toBe('Emote not found');
         }
     });
 
     test('Fetch global emotes', async() => {
-        const emotes = await api.getGlobalEmotes();
+        const emotes = await SevenTV.getGlobalEmotes();
         expect(emotes.length).toBeGreaterThan(0);
     });
 
     test('Fetch badges', async() => {
-        const badges = await api.getBadges('twitch_id');
+        const badges = await SevenTV.getBadges('twitch_id');
         expect(badges.length).toBeGreaterThan(0);
     });
 
-    test('Fetch badges with invalid id', async() => {
-        try {
-            await api.getBadges('invalid');
-        } catch (e) {
-            expect(e.message).toBe('Invalid user_identifier');
-        }
-    });
-
     test('Fetch paints', async() => {
-        const paints = await api.getPaints('twitch_id');
+        const paints = await SevenTV.getPaints('twitch_id');
         expect(paints.length).toBeGreaterThan(0);
-    });
-
-    test('Fetch paints with invalid id', async() => {
-        try {
-            await api.getPaints('invalid');
-        } catch (e) {
-            expect(e.message).toBe('Invalid user_identifier');
-        }
     });
 });
